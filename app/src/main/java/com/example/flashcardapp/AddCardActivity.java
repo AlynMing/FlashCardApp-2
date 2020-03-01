b8615198d90799f5b1a3f75cde9a99850ef76143
Faruq Hammed <faruqhammed8@gmail.com>
2020-03-01 05:10:01 -0600

    added multiple extra features

--- app/src/main/java/com/example/flashcardapp/AddCardActivity.java
+++ app/src/main/java/com/example/flashcardapp/AddCardActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class AddCardActivity extends AppCompatActivity {
 
@@ -24,15 +28,36 @@ public class AddCardActivity extends AppCompatActivity {
         findViewById(R.id.save_new_card).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String question = ((EditText) findViewById(R.id.question)).getText().toString();
-                String answer = ((EditText) findViewById(R.id.answer)).getText().toString();
 
-                Intent getNew = new Intent();
-                getNew.putExtra("question", question);
-                getNew.putExtra("answer", answer);
-                setResult(RESULT_OK, getNew);
-                finish();
+                ArrayList<String> answers = new ArrayList<>();
+                String question = ((EditText) findViewById(R.id.question)).getText().toString();
+
+                answers.add(((EditText) findViewById(R.id.answer1)).getText().toString());
+                answers.add(((EditText) findViewById(R.id.answer2)).getText().toString());
+                answers.add(((EditText) findViewById(R.id.answer3)).getText().toString());
+
+                if(!isValid(answers, question)){
+                    Toast.makeText(getApplicationContext(), "Must enter data into all fields", Toast.LENGTH_SHORT).show();
+                }else{
+                    Intent getNew = new Intent();
+                    getNew.putExtra("question", question);
+                    getNew.putExtra("answers", answers);
+                    getNew.putExtra("correctAnswer", answers.get(0));
+                    setResult(RESULT_OK, getNew);
+                    finish();
+                }
+
             }
         });
+    }
+
+    private boolean isValid(ArrayList<String> answers, String question){
+        for(String x: answers){
+            if(x.matches("")) return false;
+        }
+
+        if(question.matches("")) return false;
+
+        return true;
     }
 }
--- app/src/main/java/com/example/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -6,28 +6,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
     boolean isShowingAnswer = false;
+    String answer = "Barack Obama";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.card_question).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                findViewById(R.id.card_question).setVisibility(View.INVISIBLE);
-                findViewById(R.id.card_answer).setVisibility(View.VISIBLE);
-                isShowingAnswer = true;
-            }
-        });
+        findViewById(R.id.answer1).setVisibility(View.INVISIBLE);
+        findViewById(R.id.answer2).setVisibility(View.INVISIBLE);
+        findViewById(R.id.answer3).setVisibility(View.INVISIBLE);
+        isShowingAnswer = false;
 
-        findViewById(R.id.add_card).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.plus_card).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent add = new Intent(MainActivity.this, AddCardActivity.class);
@@ -39,28 +39,77 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(isShowingAnswer){
-                    findViewById(R.id.card_answer).setVisibility(View.INVISIBLE);
-                    findViewById(R.id.card_question).setVisibility(View.VISIBLE);
+                    findViewById(R.id.answer1).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.answer2).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.answer3).setVisibility(View.INVISIBLE);
                     isShowingAnswer = false;
+
+                    //change colors
+                    findViewById(R.id.answer1).setBackgroundColor(getResources().getColor(R.color.ans1));
+                    findViewById(R.id.answer2).setBackgroundColor(getResources().getColor(R.color.ans2));
+                    findViewById(R.id.answer3).setBackgroundColor(getResources().getColor(R.color.ans3));
+
                 }
                 else{
-                    findViewById(R.id.card_answer).setVisibility(View.VISIBLE);
-                    findViewById(R.id.card_question).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.answer1).setVisibility(View.VISIBLE);
+                    findViewById(R.id.answer2).setVisibility(View.VISIBLE);
+                    findViewById(R.id.answer3).setVisibility(View.VISIBLE);
+
                     isShowingAnswer = true;
                 }
             }
         });
+
+        findViewById(R.id.answer1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView t = findViewById(R.id.answer1);
+                checkCorrect(t);
+            }
+        });
+
+        findViewById(R.id.answer2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView t = findViewById(R.id.answer2);
+                checkCorrect(t);
+            }
+        });
+
+        findViewById(R.id.answer3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView t = findViewById(R.id.answer3);
+                checkCorrect(t);
+            }
+        });
+    }
+
+    private void checkCorrect(TextView t){
+        if(answer.equals(t.getText().toString())) {
+            t.setBackgroundColor(getResources().getColor(R.color.green));
+        }else{
+            t.setBackgroundColor(getResources().getColor(R.color.red));
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if(requestCode == 1 && resultCode == RESULT_OK){
-            String q = data.getExtras().getString("question");
-            String a = data.getExtras().getString("answer");
 
-            ((TextView) findViewById(R.id.card_question)).setText(q);
-            ((TextView) findViewById(R.id.card_answer)).setText(a);
-            System.out.println("here");
+            ArrayList<String> answers = data.getExtras().getStringArrayList("answers");
+
+            Collections.shuffle(answers);
+
+            ((TextView) findViewById(R.id.card_question)).setText(data.getExtras().getString("question"));
+            ((TextView) findViewById(R.id.answer1)).setText(answers.get(0));
+            ((TextView) findViewById(R.id.answer2)).setText(answers.get(1));
+            ((TextView) findViewById(R.id.answer3)).setText(answers.get(2));
+
+            answer = data.getExtras().getString("correctAnswer");
+
+            Toast.makeText(getApplicationContext(), "Card Successfully Changed!", Toast.LENGTH_SHORT).show();
+
         }
     }
 }
